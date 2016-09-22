2438313b22ce86302565f44aa9e3006145fc8065
kfrederick5 <tefsuncle@gmail.com>
2016-09-22 16:30:30 -0700

    Added to QuizActivity

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/QuizActivity.java
@@ -1,15 +1,22 @@
 package edu.orangecoastcollege.cs273.kfrederick5.flagquiz;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class QuizActivity extends AppCompatActivity {
+
+    public static final String CHOICES = "pref_numberOfChoices";
+    public static final String Regions = "pref_regionsToInclude";
+
+    private boolean phoneDevice = true; // Used to force portrait mode
+    private boolean preferencesChanged = true; // did preferences change?
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,25 @@ public class QuizActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        // set default values in the app's SharedPreferences
+        PreferenceManager.setDefaultValues(this,R.xml.preferences, false);
+
+        // register listener for SharedPreferences changes
+        PreferenceManager.getDefaultSharedPreferences(this).
+                registerOnSharedPreferenceChangeListener(preferencesChangeListener);
+
+        //determine screen size
+        int screenSize = getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK;
+
+        //if device is a tablet, set phoneDevice to false
+        if (screenSize == Configuration.SCREENLAYOUT_SIZE_LARGE ||
+                screenSize == Configuration.SCREENLAYOUT_SIZE_XLARGE)
+            phoneDevice = false; // not a phone-sized device
+
+        //if running on phone-sized device, allow only portrait orientation
+        if(phoneDevice)
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
     @Override
