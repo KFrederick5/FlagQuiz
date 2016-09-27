8d9e7c19be4cca21f3c573fcbeffe4c7ed0e3bb0
kfrederick5 <tefsuncle@gmail.com>
2016-09-26 18:39:10 -0700

    Finished adding tutorial source code information.

--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/QuizActivity.java
@@ -1,22 +1,38 @@
 package edu.orangecoastcollege.cs273.kfrederick5.flagquiz;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import java.util.Set;
+
 
 public class QuizActivity extends AppCompatActivity {
 
     public static final String CHOICES = "pref_numberOfChoices";
-    public static final String Regions = "pref_regionsToInclude";
+    public static final String REGIONS = "pref_regionsToInclude";
 
     private boolean phoneDevice = true; // Used to force portrait mode
     private boolean preferencesChanged = true; // did preferences change?
 
+
+    /**
+     * onCreate generates the appropriate layout to inflate, depending on the screen size.
+     * If the device is large or XL, it will load the content_main.xml (sw700dp-land) which
+     * includes both the fragment_quiz.xml and fragment_settings.xml. Otherwise, it just
+     * inflates the standard content_main.xml with the fragment_quiz.
+     *
+     * All default preferences are set using the preferences.xml file.
+     * @param savedInstanceState The saved state to restore (not being used)
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,25 +62,111 @@ public class QuizActivity extends AppCompatActivity {
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
+    /**
+     * onStart is called after onCreate completes execution. This method will update the number
+     * of guess rows to display and the regions to choose flags from, then resets the
+     * quiz with the new preferences.
+     */
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        if(preferencesChanged){
+            // now that the default preferences have been set,
+            //initialize QuizActivityFragment and start the quiz
+            QuizActivityFragment quizFragment = (QuizActivityFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.quizFragment);
+            quizFragment.updateGuessRows(
+                    PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.updateRegions(
+                    PreferenceManager.getDefaultSharedPreferences(this));
+            quizFragment.resetQuiz();
+            preferencesChanged = false;
+
+        }
+    }
+
+    /**
+     * Shows the settings menu if the app is running on a phone or a portrait-oriented
+     * tablet only. (Large screen sizes include the settings fragment in the layout)
+     *
+     * @param menu The sttings menu.
+     * @return True if the sttings menu was inflated, false otherwise.
+     */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
-        return true;
-    }
+        int orientation = getResources().getConfiguration().orientation;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if(orientation == Configuration.ORIENTATION_PORTRAIT) {
+            getMenuInflater().inflate(R.menu.menu_quiz, menu);
             return true;
         }
+        else
+            return false;
+    }
 
+    /**
+     * Displays the SettingsActivity when running on a phone or portrait-oriented tablet.
+     * Starts the activity by use of an Intent (no data passed because the shared
+     * preferences, preferences.xml, has all data necessary)
+     *
+     * @param item The menu item.
+     * @return True if an option item was selected.
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent preferencesIntent = new Intent(this, SettingsActivity.class);
+        startActivity(preferencesIntent);
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * Listener to handle changes in the app's shared preferences (preferences.xml).
+     *
+     * If either the guess options or regions are changed, the quiz will restart with the
+     * new settings.
+     */
+
+    private SharedPreferences.OnSharedPreferenceChangeListener preferencesChangeListener =
+            new SharedPreferences.OnSharedPreferenceChangeListener() {
+
+                //called when the user changes the app's preferences
+                @Override
+                public void onSharedPreferenceChanged(
+                        SharedPreferences sharedPreferences, String key) {
+                    preferencesChanged = true; //user changed app setting
+
+                    QuizActivityFragment quizFragment = (QuizActivityFragment)
+                            getSupportFragmentManager().findFragmentById(
+                                    R.id.quizFragment);
+
+                    if (key.equals(CHOICES)) { // # of choices to display changed
+                        quizFragment.updateGuessRows(sharedPreferences);
+                        quizFragment.resetQuiz();
+                    }
+                    else if (key.equals(REGIONS)) {// regions to include changed
+                        Set<String> regions =
+                                sharedPreferences.getStringSet(REGIONS, null);
+
+                        if (regions != null && regions.size() > 0) {
+                            quizFragment.updateRegions(sharedPreferences);
+                            quizFragment.resetQuiz();
+                        } else {
+                            //must select one region--set North America as default
+                            SharedPreferences.Editor editor =
+                                    sharedPreferences.edit();
+                            regions.add(getString(R.string.default_region));
+                            editor.putStringSet(REGIONS, regions);
+                            editor.apply();
+
+                            Toast.makeText(QuizActivity.this,
+                                    R.string.default_region_message,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            };
+
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/SettingsActivityFragment.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kfrederick5/flagquiz/SettingsActivityFragment.java
@@ -1,22 +1,17 @@
 package edu.orangecoastcollege.cs273.kfrederick5.flagquiz;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.PreferenceFragment;
 
 /**
- * A placeholder fragment containing a simple view.
+ * SettingsActivityFragment is a subclass of PreferenceFragment for managing
+ * the app settings, such as number of guesses and regions to select flags from.
  */
-public class SettingsActivityFragment extends Fragment {
-
-    public SettingsActivityFragment() {
-    }
-
+public class SettingsActivityFragment extends PreferenceFragment {
+    //creates preferences GUI from preferences.xml file in res/xml
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        addPreferencesFromResource(R.xml.preferences); // load from XML
     }
 }
